4288f2ca9bd7ab1d2c5300819c277f3dc82dfd1a
suhas <suhas.r@codemonk.in>
2021-03-02 17:13:03 +0530

    Feat: Using Axios for sending http requests

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
 
 import './FullPost.css';
+import axios from '../../axios';
 
 class FullPost extends Component {
+    state= {
+        title:null,
+        body:null,
+        id: null,
+        error:false
+    }
+    componentDidUpdate() {
+        if((this.props.id && this.state.id === null && !this.state.error)|| (this.state.id !== this.props.id && !this.state.error)){
+            axios.get("/posts/"+ this.props.id).then(
+                response => {
+                    this.setState({title:response.data.title,body:response.data.body,id:response.data.id})
+                }
+            ).catch(error => {
+                    this.setState({error:true})
+            })
+        }
+    }
     render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+        let post = <p>loading</p>
+        if (this.state.error){
+            post= <p>somethign went wrong</p>
 
-        );
+        }
+        else{
+           post =  this.props.id ?(
+                <div className="FullPost">
+                    <h1>{this.state.title}</h1>
+                    <p>{this.state.body}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+    
+            ): <p style={{textAlign: "center"}}>Please select a Post!</p>
+        }
         return post;
     }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,18 +4,49 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
-
+import Axios from 'axios'
 class Blog extends Component {
+    state = {
+        posts: [],
+        selectedId : null
+    }
+    componentDidMount(){
+        Axios.get("/posts").then(
+            response => {
+                const shortData = response.data.slice(0,4)
+                const updatedData = shortData.map(post =>(
+                    {
+                        ...post,
+                        author:"suhas"
+                    }
+                ))
+                this.setState({posts:updatedData})
+                }
+                
+                
+            
+        )
+    }
+    postDetailhandler = (id) => {
+        this.setState({selectedId:id})
+    }
+
     render () {
+        let posts  = this.state.posts.map(
+            post => (
+                <Post title={post.title} 
+                key={post.id} 
+                clicked={()=>this.postDetailhandler(post.id)}
+                author={post.author}/>
+            )
+        )
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedId} />
                 </section>
                 <section>
                     <NewPost />
